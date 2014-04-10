e1a9fbc240e8c009fc29dd7ba497c2d268544f8f
laerke <TEST@TESTs-MacBook-Pro.local>
2014-04-10 17:20:57 +0200

    Directions
    Har prøvet at lave koden for alle directions. Men den bliver ved med at
    komme op med en fejl, jeg ikke ved hvordan jeg skal rette. Måske nogle
    af jer andre har en idé?

--- New_Pac_Man/Assets/Script/Movement.cs
+++ New_Pac_Man/Assets/Script/Movement.cs
@@ -4,23 +4,38 @@ using System.Collections;
 public class Movement : MonoBehaviour {
 
 	int speed = 5;
-	private Vector2 direction;
+	private Vector2 directionUp;
+	private Vector2 directonDown;
+	private Vector2 directonLeft;
+	private Vector2 directonRight;
 
 	// Use this for initialization
 	void Start () {
 
-		direction = new Vector2(10,10);
-		//TEST
-			
-
+		directionUp = new Vector2 (10,10);
+		directionDown = new Vector2 (-10, -10);
+		directionLeft = new Vector2 (10,0);
+		directionRight = new Vector2 (-10, 0);
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetKey (KeyCode.UpArrow)) {
-			rigidbody2D.velocity = direction;
-			//rigidbody2D.velocity.y = y;
+			rigidbody2D.velocity = directionUp;
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			rigidbody2D.velocity = directionDown;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rigidbody2D.velocity = directionLeft;
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rigidbody2D.velocity = directionRight;
 		}
 	}
 }
