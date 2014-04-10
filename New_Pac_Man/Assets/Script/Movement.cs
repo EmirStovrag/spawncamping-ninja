abd14e1669d05238f75bf7dcd634f1fec6effb75
Emir Stovrag <emir_s4@hotmail.com>
2014-04-10 13:34:20 +0200

    Movement
    Huan her er et movement script, som funger med collider.

---
+++ New_Pac_Man/Assets/Script/Movement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	int speed = 5;
+	private Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+
+		direction = new Vector2(10,10);
+	
+			
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rigidbody2D.velocity = direction;
+			//rigidbody2D.velocity.y = y;
+		}
+	}
+}
