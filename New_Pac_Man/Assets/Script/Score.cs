bdb84cc65206315e506b1c4c0dcc79a72c7ee183
Huan Huu Hoang <huanhoang@hotmail.com>
2014-04-29 18:19:54 +0200

    Too many fuck ups
    I had to delete all and fix the most from local driver

---
+++ New_Pac_Man/Assets/Script/Score.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Score : MonoBehaviour {
+
+    public int finalScore = 1;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        finalScore = GameObject.Find("Pacman").GetComponent<Movement>().score;
+        guiText.text = "Score " + finalScore;
+	}
+}
