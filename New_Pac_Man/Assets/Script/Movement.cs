05fa004dd0c16c5f42a827605a2d02de4888ebfe
Huan Huu Hoang <huanhoang@hotmail.com>
2014-04-29 18:20:08 +0200

    Same as before
    same as before

---
+++ New_Pac_Man/Assets/Script/Movement.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public KeyCode moveUp;
+	public KeyCode moveDown;
+	public KeyCode moveLeft;
+	public KeyCode moveRight;
+
+	public float speed = 2;
+
+	public Sprite spriteUp, spriteDown, spriteLeft, spriteRight;
+	private SpriteRenderer spriteRenderer;
+
+	public int status = 1;
+
+	public int score;
+
+    Timer timerRef;
+
+	// Use this for initialization
+	void Start () {
+        timerRef = GameObject.Find("Timer").GetComponent<Timer>();
+		spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (moveUp)) { 
+						rigidbody2D.velocity = new Vector2 (0, speed);
+			spriteRenderer.sprite = spriteUp;
+				} else if (Input.GetKey (moveDown)) {
+						rigidbody2D.velocity = new Vector2 (0, speed * -1);
+			spriteRenderer.sprite = spriteDown;
+				} else if (Input.GetKey (moveLeft)) {
+						rigidbody2D.velocity = new Vector2 (speed * -1, 0);
+			spriteRenderer.sprite = spriteLeft;
+				} else if (Input.GetKey (moveRight)) {
+						rigidbody2D.velocity = new Vector2 (speed, 0);
+			spriteRenderer.sprite = spriteRight;
+				}
+	}
+    void OnCollisionStay2D(Collision2D collider)
+    {
+        if (collider.gameObject.tag == "Killer") {          
+            timerRef.StartTimer();
+            StartCoroutine(startUpdateStatus(collider));
+            Destroy(collider.gameObject);
+        }
+    }
+    
+    IEnumerator startUpdateStatus(Collision2D collider){
+        while (GameObject.Find("Timer").GetComponent<Timer>().GetIsOn() == true)
+        {
+            status = 2;
+            yield return new WaitForEndOfFrame();
+        }
+        status = 1;
+   }
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.tag == "Point") {
+			Destroy (other.gameObject);
+			score += 10;
+		}
+	}
+}
